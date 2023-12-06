56d6c59b3d24aabd1720dc26c7abd92a93684508
agoktugaydin <goktugaydin36@gmail.com>
2023-12-06 16:30:27 +0300

    big feat: add create, update and send data methods

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,64 @@
 #include <ArduinoJson.h>
 #include <config.h>
 
-const char* ssid =  WIFI_SSID;
-const char* password = WIFI_PASSWORD;
 
 // IP address and port number of the WebSocket server
-IPAddress webSocketServer(192, 168, 1, 36);
-int webSocketPort = 5600;
+IPAddress webSocketServer(WS_SERVER);
+int webSocketPort = WS_PORT;
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
 
 WebSocketsClient wsClient;
 
-void sendData() {
-  // JSON object to hold the data
-  StaticJsonDocument<200> jsonDoc;
-  
-  jsonDoc["message"] = "Hello from ESP32";
-  jsonDoc["millis"] = millis();
+// JSON object to hold the data
+StaticJsonDocument<300> jsonDoc;
 
-  // Serialize the JSON to a buffer
-  char buffer[200];
-  serializeJson(jsonDoc, buffer);
+void updateSlaveData(const String& masterId, const String& slaveId, float gasDensity, float temperature) {
+  // Iterate over each master object in the JSON document
+  for (JsonObject master : jsonDoc.as<JsonArray>()) {
+    // Check if the master ID matches the specified masterId
+    if (master["master_id"].as<String>() == masterId) {
+      // Iterate over each slave object within the master
+      for (JsonObject slave : master["slaves"].as<JsonArray>()) {
+        // Check if the slave ID matches the specified slaveId
+        if (slave["slave_id"].as<String>() == slaveId) {
+          // Access the data object within the slave and update the gasDensity and temperature values
+          JsonObject slaveData = slave["data"].as<JsonObject>();
+          slaveData["gasDensity"] = gasDensity;
+          slaveData["temperature"] = temperature;
+          return; // Exit the function after updating the data
+        }
+      }
+    }
+  }
+}
+
+void generateFakeData() {
+  // Generate random gasDensity and temperature values
+  float gasDensity = random(20, 30) + random(0, 10) / 10.0;
+  float temperature = random(40, 60) + random(0, 10) / 10.0;
+
+  // Update slave data for master ID: "1", slave ID: "1"
+  updateSlaveData("1", "1", gasDensity, temperature);
+
+  // Update slave data for master ID: "1", slave ID: "2"
+  gasDensity = random(20, 30) + random(0, 10) / 10.0;
+  temperature = random(40, 60) + random(0, 10) / 10.0;
+  updateSlaveData("1", "2", gasDensity, temperature);
+}
+
+
+void sendData() {
+  // Generate fake data
+  generateFakeData();
+
+  // Serialize the JSON data
+  String jsonData;
+  serializeJson(jsonDoc, jsonData);
 
   // Send the JSON data over WebSocket
-  wsClient.sendTXT(buffer);
+  wsClient.sendTXT(jsonData);
+  Serial.println(jsonData);
 }
 
 
@@ -49,6 +85,28 @@ void setup() {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
+
+  // Clear the JSON document
+  jsonDoc.clear();
+
+
+  // Add an empty array to the JSON document
+  JsonArray masters = jsonDoc.to<JsonArray>();
+
+  // Add master objects to the array
+  JsonObject master1 = masters.createNestedObject();
+  master1["master_id"] = "1";
+  JsonArray slaves1 = master1.createNestedArray("slaves");
+  JsonObject slave1 = slaves1.createNestedObject();
+  slave1["slave_id"] = "1";
+  JsonObject slaveData1 = slave1.createNestedObject("data");
+  slaveData1["gasDensity"] = 0.0;
+  slaveData1["temperature"] = 0.0;
+  JsonObject slave2 = slaves1.createNestedObject();
+  slave2["slave_id"] = "2";
+  JsonObject slaveData2 = slave2.createNestedObject("data");
+  slaveData2["gasDensity"] = 0.0;
+  slaveData2["temperature"] = 0.0;
 
   // Start WebSocket connection and specify the event function
   wsClient.begin(webSocketServer, webSocketPort);
